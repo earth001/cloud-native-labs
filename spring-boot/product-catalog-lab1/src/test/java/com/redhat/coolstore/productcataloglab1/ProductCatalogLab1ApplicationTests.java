638904d63a9b6f1af0dce7f39599f162e15a4050
Juan Moreno <earth001@gmail.com>
2019-05-29 10:21:19 -0300

    FEAT Added lab1 solution

--- spring-boot/product-catalog-lab1/src/test/java/com/redhat/coolstore/productcataloglab1/ProductCatalogLab1ApplicationTests.java
+++ spring-boot/product-catalog-lab1/src/test/java/com/redhat/coolstore/productcataloglab1/ProductCatalogLab1ApplicationTests.java
@@ -1,16 +1,28 @@
 package com.redhat.coolstore.productcataloglab1;
 
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import com.redhat.coolstore.productcataloglab1.service.ProductCatalogService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("dev")
 public class ProductCatalogLab1ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Autowired
+  private ProductCatalogService productCatalogService;
 
+  @Test
+  public void testDefaultProductList() {
+    String message = productCatalogService.sayHello();
+    assertTrue(message != null);
+    assertEquals(message, "Hey Developer!");
+  }
 }
